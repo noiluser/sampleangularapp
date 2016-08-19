eb5a07cf9e04f2b81a46c0b8fb8dfb3b0d4545cf
srg <srg.post@gmail.com>
2016-08-19 12:59:10 +0700

    loginlogout

--- public/js/controllers/LoginController.js
+++ public/js/controllers/LoginController.js
@@ -1,10 +1,33 @@
 app.controller("authCtrl", function($scope) {
 	$scope.authText = "Log in";
+	$scope.userName = "";
+	$scope.userLastName = "";
+	$scope.userHref = "";
+	$scope.loginManage = $scope.login();
+	
 	$scope.login = function() {
 		VK.Auth.login(function (response) {
 			if (response.session) {
-				console.log(response);
-			  };
+				$scope.authText = "Log out";
+				$scope.userName = response.session.user.first_name;
+				$scope.userLastName = response.session.user.last_name;
+				$scope.userHref = response.session.user.href;
+				$scope.loginManage = $scope.logout;
+				//console.log(response);
+			};
 		}, VK.access.GROUPS);
+	};
+	
+	$scope.logout = function() {
+		VK.Auth.logout(function (response) {
+			//if (response.session) {
+				$scope.authText = "Log in";
+				$scope.userName = "";
+				$scope.userLastName = "";
+				$scope.userHref = "";				
+				$scope.loginManage = $scope.login;
+				console.log(response);
+			//};
+		});
 	};
 });
