3783dae9caf20ef565326abf958a2d86dc26cf7d
srg <srg.post@gmail.com>
2016-08-19 17:13:47 +0700

    fetching groups

--- public/js/controllers/GroupsController.js
+++ public/js/controllers/GroupsController.js
@@ -1,24 +1,38 @@
-app.controller("groupsCtrl", function($scope, $rootScope) {
+app.controller("groupsCtrl", function($scope, $http) {
 	$scope.isUserLoggedIn = false;
 	$scope.isGroupsLoading = false;
 
-	$scope.$on('userLogin', function(event, user) { 
+	$scope.$on('userLogin', function(event, user) {
+		console.log("User logged in", user);
 		$scope.isUserLoggedIn = true;
 		$scope.isGroupsLoading = true;
-		
-		console.log("loading", user);
+		$scope.user = user;
+
 		$scope.$emit('groupsLoad', user);
 	});
 	
-	$scope.$on('userLogout', function(event, data) { 
+	$scope.$on('userLogout', function(event, data) {
+		console.log("User logged out", $scope.user);
+		
 		$scope.isUserLoggedIn = false;
 		$scope.isGroupsLoading = false;
 		
+		$scope.user = {};
 		
 	});	
 	
 	$scope.$on('groupsLoad', function(event, user) { 
-		console.log("loading", user); 
+		console.log("loading groups");
+		
+		var uid = $scope.user.id;
+		$http({
+	        method : "POST",
+	        url : "https://api.vk.com/method/groups.get?user_ids=" + uid
+	    }).then(function mySucces(response) {
+	        console.log(response);
+	    }, function myError(response) {
+	    	console.log(response);
+	    });
 	});
 
 });
