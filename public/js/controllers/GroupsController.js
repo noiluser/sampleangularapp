ad7757936b54e3c7598cd2760803a070cf4100d8
Sergey <Sergey@Zanoza>
2016-08-21 23:56:49 +0700

    load groups logic

--- public/js/controllers/GroupsController.js
+++ public/js/controllers/GroupsController.js
@@ -3,8 +3,12 @@ app.controller("groupsCtrl", function($scope, $http) {
 	$scope.isGroupsLoading = false;
 	$scope.offset = 0;
 	$scope.count = 10;
+	$scope.IsGroupsLoaded = true;
+
 	
-	$scope.getGroups = function() {
+	$scope.getGroups = function(next) {
+		$scope.offset += $scope.count;
+		
 		VK.Api.call('groups.get', {
 				offset : $scope.offset, 
 				count : $scope.count, 
@@ -15,7 +19,11 @@ app.controller("groupsCtrl", function($scope, $http) {
 				if(r.response) {
 					console.log(r.response);
 					var gr = r.response;
-					var count = gr.shift();
+					var co = gr.shift();
+					if (co < $scope.count)
+						$scope.$apply(function(){
+							$scope.IsGroupsLoaded = true;
+						});
 					$scope.$emit('groupsLoaded', {"count" : count, "groups" : gr});
 				}
 		});
@@ -46,9 +54,12 @@ app.controller("groupsCtrl", function($scope, $http) {
 		console.log("Groups loaded");
 		$scope.$apply(function(){
 			$scope.isGroupsLoading = false;
+			$scope.IsGroupsLoaded = false;
 			$scope.groups = groups.groups;
 			console.log($scope.groups);
 		});
 	});
 
+	//$scope.groups = [{name : "Aaa", description : "Bbbb bbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbb b bbbbbbbbbbbbbbbbb", photo: "http://cs624416.vk.me/v624416877/1231a/OZ7c25mYgRA.jpg"},
+	//                 {name : "Ccc", description : "Ddd dddddddddddddddddd<br>dd dddddddddddddddd dddddddddddddddddd", photo: "http://cs10407.vk.me/g59184/c_b17a5775.jpg"}];
 });
