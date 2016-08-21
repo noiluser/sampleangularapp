ad7470b399b5232ec2a59c544a3a9414a8995f9b
Sergey <Sergey@Zanoza>
2016-08-22 01:03:10 +0700

    details

--- public/js/controllers/DetailsController.js
+++ public/js/controllers/DetailsController.js
@@ -1,3 +1,14 @@
 app.controller("detailsCtrl", function($scope, $routeParams) {
-	$scope.msg = $routeParams.group;
+	$scope.id = $routeParams.group;
+	
+	VK.Api.call('groups.getById', {
+		group_id : $scope.id
+	}, function(r) {
+		if(r.response) {
+			//console.log(r.response);
+			$scope.$apply(function(){
+				$scope.msg = r.response;
+			});
+		}
+});
 });
