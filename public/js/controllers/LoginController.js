7135e032e629e0d714cb00cf017067ebbfc60e8e
Sergey <Sergey@Zanoza>
2016-08-22 01:34:07 +0700

    4

--- public/js/controllers/LoginController.js
+++ public/js/controllers/LoginController.js
@@ -3,14 +3,6 @@ app.controller("authCtrl", function($scope) {
 		var access = 262144;
 		VK.Auth.login(function (response) {
 			if (response.session) {
-				/*$scope.$apply(function(){
-					$scope.userName = response.session.user.first_name;
-					$scope.userLastName = response.session.user.last_name;
-					$scope.userHref = response.session.user.href;
-					$scope.isUserLoggedIn = true;
-					
-					$scope.$parent.$broadcast('userLogin', response.session);
-				 });*/
 				$scope.$emit('userExists', response.session.user);
 			};
 		}, access);
@@ -26,12 +18,10 @@ app.controller("authCtrl", function($scope) {
 	};
 	
 	$scope.$on('userExists', function(event, user) { 
-		$scope.$apply(function(){
-			$scope.userName = user.first_name;
-			$scope.userLastName = user.last_name;
-			$scope.userHref = user.href;
-			$scope.isUserLoggedIn = true;
-		});
+		$scope.userName = user.first_name;
+		$scope.userLastName = user.last_name;
+		$scope.userHref = user.href;
+		$scope.isUserLoggedIn = true;
 		$scope.$parent.$broadcast('userLogin', user);
 	});
 	
