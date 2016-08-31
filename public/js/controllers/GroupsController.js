184142dcd5cab755759a11ae230f7cd3d2b6463f
srg <srg.post@gmail.com>
2016-08-31 16:19:49 +0700

    fix006.6

--- public/js/controllers/GroupsController.js
+++ public/js/controllers/GroupsController.js
@@ -32,18 +32,18 @@ app.controller("groupsCtrl", function($scope, $sce, $http, User, PagesService) {
 		$http.jsonp(url).
 		    success(function(data, status, headers, config) {
 
-				var gr = data.response;
-				var co = gr.shift();
+				var gr = data.response.items;
+				var co = data.response.count;
 				
 				PagesService.reload = false;
 				PagesService.offset += co;
 								
-				if (co < $scope.count)
+				/*if (co < $scope.count)
 					$scope.$apply(function(){
 						$scope.IsAllGroupsLoaded = true;
 					});
-				
-				//$scope.$emit('groupsLoaded', gr);
+				*/
+				$scope.$emit('groupsLoaded', gr);
 		    }).
 		    error(function(data, status, headers, config) {
 		        console.log(data);
