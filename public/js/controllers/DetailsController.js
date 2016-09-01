c979a6be54c7f1f30ecd5a75e4f988e35ae497d5
srg <srg.post@gmail.com>
2016-09-01 16:54:05 +0700

    details

--- public/js/controllers/DetailsController.js
+++ public/js/controllers/DetailsController.js
@@ -10,13 +10,18 @@ app.controller("detailsCtrl", function($scope, $routeParams, $http, PagesService
 	$scope.getNoteInfo = function() {
 		var getParams = {
 				note_id : this.id,
+				need_wiki : 1
 		};
 				
-		var url = "https://api.vk.com/method/groups.getById?" + this.paramsToString(getParams, true) + User.getUrlParams();
+		var url = "https://api.vk.com/method/notes.getById?" + this.paramsToString(getParams, true) + User.getUrlParams();
 		var self = this;
 		$http.jsonp(url).
 		    success(function(data, status, headers, config) {
-		    	console.log(data);
+		    	$scope.date = data.response.date;
+		    	$scope.title = data.response.title;
+		    	$scope.text = data.response.text;
+		    	$scope.text_wiki = data.response.text_wiki;
+		    	$scope.url = data.response.view_url;
 		    }).
 		    error(function(data, status, headers, config) {
 		        console.log(data);
