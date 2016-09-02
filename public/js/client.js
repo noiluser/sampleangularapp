f516bcdac4bc57206449fc115cf8b413aff5c180
Sergey <Sergey@Zanoza>
2016-09-02 18:48:03 +0700

    debug7

--- public/js/client.js
+++ public/js/client.js
@@ -1,19 +1,37 @@
 
-var app = angular.module("vkSample", ['ngRoute', 'ui.bootstrap']);
+var app = angular.module("vkSample", ['ngRoute', 'ui.bootstrap', 'ngSanitize']);
 
-app.directive('compileTemplate', ["$compile", "$parse", function($compile, $parse) {
-    return {
-        restrict: 'A',
-        link: function($scope, element, attr) {
-            var parse = $parse(attr.ngBindHtml);
-            function value() { return (parse($scope) || '').toString(); }
+app.directive('contenteditable', ['$sce', function($sce) {
+	  return {
+		    restrict: 'A', // only activate on element attribute
+		    require: '?ngModel', // get a hold of NgModelController
+		    link: function(scope, element, attrs, ngModel) {
+		      if (!ngModel) return; // do nothing if no ng-model
 
-            $scope.$watch(value, function() {
-                $compile(element, null, -9999)($scope); 
-            });
-        }
-    }
-}]);  
+		      // Specify how UI should be updated
+		      ngModel.$render = function() {
+		        element.html($sce.getTrustedHtml(ngModel.$viewValue || ''));
+		      };
+
+		      // Listen for change events to enable binding
+		      element.on('blur keyup change', function() {
+		        scope.$evalAsync(read);
+		      });
+		      read(); // initialize
+
+		      // Write data to the model
+		      function read() {
+		        var html = element.html();
+		        // When we clear the content editable the browser leaves a <br> behind
+		        // If strip-br attribute is provided then we strip this out
+		        if ( attrs.stripBr && html == '<br>' ) {
+		          html = '';
+		        }
+		        ngModel.$setViewValue(html);
+		      }
+		    }
+		  };
+		}]); 
 
 app.config(function($routeProvider, $locationProvider) {
     $routeProvider
