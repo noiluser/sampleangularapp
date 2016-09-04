5cf2f4630c138c2ff6e6fa89f28a6625f6c2390f
Sergey <Sergey@Zanoza>
2016-09-05 02:55:25 +0700

    details ready

--- public/js/controllers/DetailsController.js
+++ public/js/controllers/DetailsController.js
@@ -4,8 +4,8 @@ app.controller("detailsCtrl", function($scope, $routeParams, $http, $location, U
 	$scope.paramsToString = $scope.$parent.paramsToString;
 	
 	$scope.id = $routeParams.id;
-	$scope.IsEditPending = true; // TODO false
-	$scope.IsDeletePending = true; // TODO false
+	$scope.IsEditPending = false;
+	$scope.IsDeletePending = false;
 	$scope.IsLoadPending = true;
 
 	$scope.getNoteInfo = function() {
